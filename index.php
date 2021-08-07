7b69f6a54d29e3181631145724c795aa6659dbff
SevenDark7 <abedim308@gmail.com>
2021-08-07 13:02:34 +0430

    add delete event using php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
         
         <div class="city">
           <label for="cityLabel">شهر</label>
-          <input type="text" id="userCity" name="userCity" placeholder="مثال: اصفهان">
+          <input type="text" id="userCity" name="userCity" autocomplete="off" placeholder="مثال: اصفهان">
         </div>
     
         <div class="btn">
@@ -42,13 +42,13 @@
     </article>
 
     <?php 
-      $Users = array( ['مهدی عابدی', 20, 'اصفهان'],
-                      ['حسین مرادی', 37, 'تهران'],
-                      ['محمد یاسینی', 24, 'یاسوج'],
-                      ['علی فتحی', 18, 'گرگان'],
-                      ['حامد میرعلائی', 27, 'کاشان'],
-                      ['محسن امینی', 43, 'کرمان']
-                    );
+      $Users = [
+        [1, 'مهدی عابدی', 20, 'اصفهان'],
+        [2, 'محمد ایزدی', 32, 'کرمان'],
+        [3, 'حامد محمدی', 18, 'تهران'],
+        [4, 'حسین حسینی', 27, 'کاشان'],
+        [5, 'علی یاسینی', 24, 'شیراز']
+      ];
 
       /////////// Write User Information In CSV File ///////////
 
@@ -61,11 +61,12 @@
         fclose($csvFile);
       }
 
-
       /////////// Get User Information From Form And Write In CSV File ///////////
 
       if (isset($_POST['userName']) && isset($_POST['userAge']) && isset($_POST['userCity'])) {
-        $info = [$_POST['userName'], $_POST['userAge'], $_POST['userCity']];
+        $csvFile = file('csv/users.csv');
+        $count = count($csvFile);
+        $info = [++$count, $_POST['userName'], $_POST['userAge'], $_POST['userCity']];
         $csvFile = fopen('csv/users.csv', 'a');
         fputcsv($csvFile, $info);
         fclose($csvFile);
@@ -88,25 +89,42 @@
         $csvFile = fopen('csv/users.csv', 'r');
         while(!feof($csvFile)) {
           $data = fgetcsv($csvFile);
-          if(empty($data))
+          if(empty($data)){
+            fclose($csvFile);
             break;
-
+          }
           echo "
             <tr>
-              <td><button type='button' class='del'>حذف</button></td>
+              <td><a href='index.php?line=$data[0]'><button type='button' class='del'>حذف</button></a></td>
               <td><button type='button' class='edit'>ویرایش</button></td>
+              <td>$data[3]</td>
               <td>$data[2]</td>
               <td>$data[1]</td>
-              <td>$data[0]</td>
             </tr>
           ";
         }
+        if (isset($_GET['line'])) {
+          $remaining = array();
+          $csvFile = fopen('csv/users.csv', 'r');
+          while(!feof($csvFile)) {
+            $row = fgetcsv($csvFile);
+            if(empty($row))
+              break;
+            elseif (($row[0] != $_GET['line']))
+              array_push($remaining, $row);
+          }
+          fclose($csvFile);
+          $csvFile = fopen('csv/users.csv', 'w');
+          foreach($remaining as $item) {
+            fputcsv($csvFile, $item);
+          }
+          fclose($csvFile);
+          header("Refresh:0; url=index.php");
+        } 
         ?>
-
 
       </tbody>
     </table>
-
 
     <!-- <script src="js/jquery-3.6.0.min.js"></script> -->
     <!-- <script src="js/main.js"></script> -->
