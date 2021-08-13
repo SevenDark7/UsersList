8c1435061f9af2a6fdde8ed606ff2af47469b18f
SevenDark7 <abedim308@gmail.com>
2021-08-13 18:11:19 +0430

    save default users in csv file

--- def.php
+++ def.php
@@ -6,6 +6,31 @@
         [4, 'حسین حسینی', 27, 'کاشان'],
         [5, 'علی یاسینی', 24, 'شیراز']
       ];
+
+    if (!file_exists('csv')) {
+      mkdir('csv');
+      $csvFile = fopen('csv/users.csv', 'w');
+      foreach ($users as $user) {
+        fputcsv($csvFile, $user);
+      }
+      fclose($csvFile);
+    }
+    else {
+      $csvFile = fopen('csv/users.csv', 'w');
+      foreach ($users as $user) {
+        fputcsv($csvFile, $user);
+      }
+      fclose($csvFile);
+    }
+
+    $users = array();
+    $csvFile = fopen('csv/users.csv', 'r');
+    while (!feof($csvFile)) {
+      $data = fgetcsv($csvFile);
+      if (empty($data))
+        break;
+      array_push($users, $data);
+    }
+    fclose($csvFile);
     echo json_encode($users);
-    
 ?>
