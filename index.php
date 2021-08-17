086becb05452ba4046084045aacbca846c570217
SevenDark7 <abedim308@gmail.com>
2021-08-18 01:49:03 +0430

    create database for users

--- index.php
+++ index.php
@@ -5,110 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title>Project2</title>
+    <script src="js/jquery-3.6.0.min.js"></script>
   </head>
   <body>
 
     <?php
 
-    // $Users = [
-    //   [1, 'مهدی عابدی', 20, 'اصفهان'],
-    //   [2, 'محمد ایزدی', 32, 'کرمان'],
-    //   [3, 'حامد محمدی', 18, 'تهران'],
-    //   [4, 'حسین حسینی', 27, 'کاشان'],
-    //   [5, 'علی یاسینی', 24, 'شیراز']
-    // ];
+/////////////////////// Connecting To Database ///////////////////////
 
-    /////////// Write User Information In CSV File ///////////
+    $connection = mysqli_connect('localhost:3306', 'root', '');
+    if (!$connection) { 
+      echo "Could not connect to the database" . mysqli_connect_error();
+    }
+/////////////////////////////////////////////////////////////////////
 
-    // if (!file_exists('csv')){
-    //   mkdir('csv');
-    //   $csvFile = fopen('csv/users.csv', 'w');
-    //   foreach ($Users as $user) {
-    //     fputcsv($csvFile, $user);
-    //   }
-    //   fclose($csvFile);
-    // }
+/////////////////////// Create Database And Table ///////////////////////
 
-
-    /////////// Add Delete And Edit Event Using PHP ///////////
-
-    // if (isset($_GET['line'])) {
-    //   $id = $_GET['line'];
-    //   if (!function_exists('str_contains')) {
-    //     function str_contains($text, $needle) {
-    //       return empty($needle) || strpos($text, $needle) !== false;
-    //     }
-    //   }
-    //   if (!str_contains($id, 'e')) {
-    //     $remaining = array();
-    //     $csvFile = fopen('csv/users.csv', 'r');
-    //     while(!feof($csvFile)) {
-    //       $row = fgetcsv($csvFile);
-    //       if(empty($row))
-    //         break;
-    //       if (($row[0] != $_GET['line']))
-    //         array_push($remaining, $row);
-    //     }
-    //     fclose($csvFile);
-    //     $csvFile = fopen('csv/users.csv', 'w');
-    //     foreach($remaining as $item) {
-    //       fputcsv($csvFile, $item);
-    //     }
-    //     fclose($csvFile);
-    //     header('Location: index.php');
-    //   }
-    //   else {
-    //     $id = trim($id, 'e');
-    //     $csvFile = fopen('csv/users.csv', 'r');
-    //     while(!feof($csvFile)) {
-    //       $row = fgetcsv($csvFile);
-    //       if(empty($row))
-    //         break;
-    //       if (($row[0] == $id)){
-    //         $etRow = $row;
-    //         break;
-    //       }
-    //     }
-    //     fclose($csvFile);
-    //   }
-    // }
-
-    // /////////// Edit User ///////////
-
-    // if (isset($_POST['userName']) && isset($_POST['userAge']) && isset($_POST['userCity'])) {
-    //   if (isset($_GET['row'])) {
-    //     $id = $_GET['row'];
-    //     $edited = [$id, $_POST['userName'], $_POST['userAge'], $_POST['userCity']];
-    //     $remaining = array();
-    //     $csvFile = fopen('csv/users.csv', 'r');
-    //     while (!feof($csvFile)) {
-    //       $line = fgetcsv($csvFile);
-    //       if (empty($line))
-    //         break;
-    //       elseif (($line[0] == $edited[0])) 
-    //         array_push($remaining, $edited);   
-    //       else 
-    //         array_push($remaining, $line);
-    //     }
-    //     fclose($csvFile);
-    //     $csvFile = fopen('csv/users.csv', 'w');
-    //     foreach($remaining as $users) {
-    //       fputcsv($csvFile, $users);
-    //     }
-    //     fclose($csvFile);
-    //     header('Location: index.php');
-    //   } /////////// Get User Information From Form And Write In CSV File ///////////
-    //   else {
-    //     $csvFile = file('csv/users.csv');
-    //     $count = count($csvFile);
-    //     $info = [++$count, $_POST['userName'], $_POST['userAge'], $_POST['userCity']];
-    //     $csvFile = fopen('csv/users.csv', 'a');
-    //     fputcsv($csvFile, $info);
-    //     fclose($csvFile);
-    //   }
-    // } 
-
+    $dataBase = mysqli_query($connection, 'CREATE DATABASE IF NOT EXISTS usersinfo');
+    if (!$dataBase) {
+      echo "Could not create database " . mysqli_error($connection);
+    }
+    mysqli_select_db($connection, 'usersinfo');
+    $SQL = "CREATE TABLE IF NOT EXISTS users (ID INT AUTO_INCREMENT PRIMARY KEY,
+     Name VARCHAR(255) NOT NULL, Age INT NOT NULL, City VARCHAR(255) NOT NULL)";
+    if (! $table = mysqli_query($connection, $SQL)) {
+      echo "Could not create table " . mysqli_error($connection);
+    }
+/////////////////////////////////////////////////////////////////////
     ?>
+
 
     <article>
       <header>
@@ -120,26 +45,25 @@
     </article>
 
     <article class="formContine">
-      <form class="inputForm" action="index.php<?php if (isset($etRow)) echo '?row=' . $etRow[0]; ?>" method="post">
+      <form class="inputForm" action="index.php" method="post">
         <div class="name">
           <label for="nameLabel">نام و نام خانوادگی</label>
-          <input type="text" id="userName" value="<?php if (isset($etRow)) echo $etRow[1]; ?>" autocomplete="off" placeholder="مثال: مهدی عابدی" autofocus> 
+          <input type="text" id="userName" autocomplete="off" placeholder="مثال: مهدی عابدی" autofocus> 
         </div>
 
         <div class="age">
           <label for="ageLabel">سن</label>
-          <input type="number" id="userAge" value="<?php if (isset($etRow)) echo $etRow[2]; ?>" placeholder="مثال: 20">
+          <input type="number" id="userAge" placeholder="مثال: 20">
         </div>
         
         <div class="city">
           <label for="cityLabel">شهر</label>
-          <input type="text" id="userCity" value="<?php if (isset($etRow)) echo $etRow[3]; ?>" autocomplete="off" placeholder="مثال: اصفهان">
+          <input type="text" id="userCity" autocomplete="off" placeholder="مثال: اصفهان">
         </div>
     
         <div class="btn">
           <button type="reset" id="reset">انصراف</button>
           <button type="submit" id="register">ثبت</button>
-          <!-- <button type="button" id="change">ویرایش</button> -->
         </div>
       </form>
     </article>
@@ -155,26 +79,6 @@
         </tr>
       </thead>
       <tbody class="users">
-
-        <?php
-        $csvFile = fopen('csv/users.csv', 'r');
-        while(!feof($csvFile)) {
-          $data = fgetcsv($csvFile);
-          if(empty($data)){
-            fclose($csvFile);
-            break;
-          }
-          echo "
-            <tr>
-              <td><a href='index.php?line=$data[0]'><button type='button' class='del'>حذف</button></a></td>
-              <td><a href='index.php?line=$data[0]'><button type='button' class='edit'>ویرایش</button></a></td>
-              <td>$data[3]</td>
-              <td>$data[2]</td>
-              <td>$data[1]</td>
-            </tr>
-          ";
-        }
-        ?>
       </tbody>
     </table>
 
