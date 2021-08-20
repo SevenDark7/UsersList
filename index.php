ea535433907c1ab101cb9ad69493f9bf227cc240
SevenDark7 <abedim308@gmail.com>
2021-08-20 14:48:17 +0430

    add edit event using sql server

--- index.php
+++ index.php
@@ -33,17 +33,50 @@
     }
 /////////////////////////////////////////////////////////////////////
 
-/////////////////////// Insert New User To Table ///////////////////////
+/////////////////////// Edit User In Table ///////////////////////
+
+    if (isset($_GET['edit'])) {
+      $id = (int)$_GET['edit'];
+      mysqli_select_db($connection, 'userinfo');
+      $stmt = mysqli_prepare($connection,"SELECT * FROM users WHERE ID = ?");
+      mysqli_stmt_bind_param($stmt, 'i', $id);
+      mysqli_stmt_execute($stmt);
+      $result = mysqli_stmt_get_result($stmt);
+      if ($result->num_rows == 0) {
+        header('Location: index.php');
+        return;
+      }
+      $user = mysqli_fetch_assoc($result);
+    }
+
+/////////////////////////////////////////////////////////////////////
+
+/////////////////////// Insert New User And Edit User To Table ///////////////////////
 
     if (isset($_POST['userName']) && isset($_POST['userAge']) && isset($_POST['userCity'])) {
-
       $name = $_POST['userName'];
       $age = (int)$_POST['userAge'];
       $city = $_POST['userCity'];
-      mysqli_select_db($connection, 'usersinfo');
-      $stmt = mysqli_prepare($connection ,"INSERT INTO users (City, Age, Name) VALUES (?, ?, ?)");
-      mysqli_stmt_bind_param($stmt, 'sis', $city, $age, $name);
-      mysqli_stmt_execute($stmt);
+      if (isset($_GET['edit'])) {
+        $id = (int)$_GET['edit'];
+        mysqli_select_db($connection, 'userinfo');
+        $stmt = mysqli_prepare($connection,"UPDATE users SET City = ?, Age = ?, Name = ? WHERE ID = ?");
+        mysqli_stmt_bind_param($stmt, 'sisi', $city, $age, $name, $id);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        if ($result->num_rows == 0) {
+          header('Location: index.php');
+          return;
+        }
+        if (mysqli_affected_rows($connection)) {
+          header('Location: index.php');
+        }
+      }else {
+        mysqli_select_db($connection, 'usersinfo');
+        $stmt = mysqli_prepare($connection ,"INSERT INTO users (City, Age, Name) VALUES (?, ?, ?)");
+        mysqli_stmt_bind_param($stmt, 'sis', $city, $age, $name);
+        mysqli_stmt_execute($stmt);
+      }
     }
 
 /////////////////////////////////////////////////////////////////////
@@ -69,13 +102,15 @@
 
 /////////////////////////////////////////////////////////////////////
 
+/////////////////////// Change Auto Increment Value ///////////////////////
+
     $usrs = mysqli_query($connection, 'SELECT * FROM users');
-    if (mysqli_num_rows($usrs) <= 0) {
+    if (mysqli_num_rows($usrs) == 0) {
       mysqli_query($connection, 'ALTER TABLE users AUTO_INCREMENT = 1');
     }
 
+/////////////////////////////////////////////////////////////////////
     ?>
-
 
     <article>
       <header>
@@ -87,20 +122,20 @@
     </article>
 
     <article class="formContine">
-      <form class="inputForm" action="index.php" method="post">
+      <form class="inputForm" action="index.php<?php if (isset($_GET['edit'])) {echo '?edit=' . $_GET['edit'];}?>" method="post">
         <div class="name">
           <label for="nameLabel">نام و نام خانوادگی</label>
-          <input type="text" id="userName" name="userName" autocomplete="off" placeholder="مثال: مهدی عابدی" autofocus> 
+          <input type="text" id="userName" name="userName" value="<?php if (isset($user)) {echo $user['Name'];}?>" autocomplete="off" placeholder="مثال: مهدی عابدی" autofocus> 
         </div>
 
         <div class="age">
           <label for="ageLabel">سن</label>
-          <input type="number" id="userAge" name="userAge" placeholder="مثال: 20">
+          <input type="number" id="userAge" name="userAge" value="<?php if (isset($user)) {echo $user['Age'];}?>" placeholder="مثال: 20">
         </div>
         
         <div class="city">
           <label for="cityLabel">شهر</label>
-          <input type="text" id="userCity" name="userCity" autocomplete="off" placeholder="مثال: اصفهان">
+          <input type="text" id="userCity" name="userCity" value="<?php if (isset($user)) {echo $user['City'];}?>" autocomplete="off" placeholder="مثال: اصفهان">
         </div>
     
         <div class="btn">
