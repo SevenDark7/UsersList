44917838969b4a754656bd197375e9231f62a806
SevenDark7 <abedim308@gmail.com>
2021-08-25 15:58:02 +0430

    change save location from csv file to database

--- signup.php
+++ signup.php
@@ -12,13 +12,35 @@
 
     <?php
     if (isset($_POST['username']) && isset($_POST['userage']) && isset($_POST['usercity'])) {
-        $csvFile = file('csv/users.csv');
-        $count = count($csvFile);
-        $info = [++$count, $_POST['username'], $_POST['userage'], $_POST['usercity']];
-        $csvFile = fopen('csv/users.csv', 'a');
-        fputcsv($csvFile, $info);
-        fclose($csvFile);
-        header('location: index.php');
+        $name = $_POST['username'];
+        $age = (int)$_POST['userage'];
+        $city = $_POST['usercity'];
+        $connection = mysqli_connect('localhost:3306', 'root', '');
+        if (!$connection) {
+            echo "Could not connect to the database" . mysqli_connect_error();
+        }
+        mysqli_select_db($connection, 'usersinfo');
+        $flag = false;
+        $users = mysqli_query($connection, 'SELECT * FROM users');
+        if ($users) {
+            while ($user = mysqli_fetch_assoc($users)) {
+                if ($user['Name'] == $name && $user['Age'] == $age && $user['City'] == $city) {
+                    $flag = true;
+                }
+            }
+        }
+        if ($flag == false) {
+            $stmt = mysqli_prepare($connection, 'INSERT INTO users (Name, Age, City) VALUES (?, ?, ?)');
+            mysqli_stmt_bind_param($stmt, 'sis', $name, $age, $city);
+            mysqli_stmt_execute($stmt);
+            header('location: index.php');
+            return;
+        }
+        else {
+            ?>
+            <script>alert("کاربری با این مشخصات یافت شد");</script>
+            <?php
+        }
     }
     ?>
 
@@ -32,15 +54,15 @@
                 <form action="signup.php" method="post" id="signup">
                     <div>
                         <i class="material-icons">person</i>
-                        <input type="text" name="username" placeholder="Your Name" autocomplete="off">
+                        <input type="text" name="username" dir="auto" placeholder="Your Name" autocomplete="off">
                     </div>  
                     <div> 
                         <i class="material-icons">date_range</i>
-                        <input type="number" name="userage" placeholder="Your Age">
+                        <input type="number" name="userage" dir="auto" placeholder="Your Age">
                     </div>
                     <div>
                         <i class="material-icons">location_city</i>
-                        <input type="text" name="usercity" placeholder="Your City" autocomplete="off">
+                        <input type="text" name="usercity" dir="auto" placeholder="Your City" autocomplete="off">
                     </div>
                     <div>
                         <input type="checkbox" checked="checked">
